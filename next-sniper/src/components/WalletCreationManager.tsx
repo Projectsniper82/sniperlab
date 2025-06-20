cafe4209eb346fb88a59991e2092706c1042ae00
projectsniper82 <shineondime@yahoo.com>
2025-06-20 12:41:39 +0200

    Use static URL for walletcreator worker

--- next-sniper/src/components/WalletCreationManager.tsx
+++ next-sniper/src/components/WalletCreationManager.tsx
@@ -18,7 +18,7 @@ export function initWalletCreationWorker(onMessage: (data: any) => void): Worker
                  try {
             console.log('[WalletCreationManager] Creating wallet worker');
             walletWorker = new Worker(
-             new URL('/workers/walletCreator.js', window.location.href),
+             new URL('../workers/walletCreator.ts', import.meta.url),
                 { type: 'module' }
             );
             console.log('[WalletCreationManager] Wallet worker created');
